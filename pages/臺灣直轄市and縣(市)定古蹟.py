d84438a20738d82cbd1c3274539de98fb1449fe9
cw75964 <cw75964@gmail.com>
2024-12-19 01:15:25 +0800

    Update 臺灣直轄市and縣(市)定古蹟.py

--- pages/臺灣直轄市and縣(市)定古蹟.py
+++ pages/臺灣直轄市and縣(市)定古蹟.py
@@ -14,13 +14,19 @@ st.title("臺灣直轄市and縣(市)定古蹟")
 
 st.header("Marker Cluster")
 old=pd.read_csv('https://github.com/cw75964/GIS-finals/raw/refs/heads/master/%E5%8F%B0%E7%81%A3%E5%8F%A4%E8%B9%9F%20%E4%BF%AE%E6%AD%A3.csv')
-nation_old=old=old[(old['assetsClassifyName']=='直轄市定古蹟')|(old['assetsClassifyName']=='縣(市)定古蹟')]
+old_na=pd.read_csv('https://github.com/cw75964/GIS-finals/raw/refs/heads/master/%E5%8F%A4%E8%B9%9F%E7%94%A8%E9%80%94%E3%80%81%E7%B8%A3%E5%B8%82.csv')
+nation_old=old[(old['assetsClassifyName']=='直轄市定古蹟')|(old['assetsClassifyName']=='縣(市)定古蹟')]
+ci_na=old_na[(old_na['assetsClassifyName']=='直轄市定古蹟')|(old_na['assetsClassifyName']=='縣(市)定古蹟')]
+ci_name=ci_na['name'].value_counts()
+ci_city=ci_na['city'].value_counts()
+
 m1 = leafmap.Map(center=[23.7652,120.4980],zoom=8,
             locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
 m1.add_basemap("OpenTopoMap")
 m1.add_points_from_xy(nation_old,x='longitude',y='latitude',spin=True,add_legend=True,layer_name='直轄市and縣(市)定古蹟')
 m1.to_streamlit(height=700)
 st.dataframe(nation_old)
+st.bar_chart(ci_name)
 st.header("Heatmap")
 nation_old['num']=10
 m2 = leafmap.Map(center=[23.7652,120.4980],zoom=8,
@@ -30,3 +36,4 @@ m2.add_basemap("OpenTopoMap")
 m2.add_heatmap(nation_old,
               latitude="latitude",longitude="longitude",value="num",name="Heat map",radius=15,)
 m2.to_streamlit(height=700)
+st.bar_chart(ci_city)
