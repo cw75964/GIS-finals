5af1740abafc829a5f31b1bfd143f871eda28fd1
cw75964 <cw75964@gmail.com>
2024-12-18 20:42:10 +0800

    Update 2_🪟_Split_Map.py

--- pages/2_🪟_Split_Map.py
+++ pages/2_🪟_Split_Map.py
@@ -8,19 +8,23 @@ Web App URL: <https://geotemplate.streamlit.app>
 GitHub Repository: <https://github.com/giswqs/streamlit-multipage-template>
 """
 
-st.sidebar.title("About")
-st.sidebar.info(markdown)
-logo = "https://i.imgur.com/UbOXYAU.png"
-st.sidebar.image(logo)
+st.title("臺灣縣(市)定古蹟")
 
-st.title("Split-panel Map")
-
-with st.expander("See source code"):
-    with st.echo():
-        m = leafmap.Map()
-        m.split_map(
-            left_layer='ESA WorldCover 2020 S2 FCC', right_layer='ESA WorldCover 2020'
-        )
-        m.add_legend(title='ESA Land Cover', builtin_legend='ESA_WorldCover')
-
-m.to_streamlit(height=700)
+st.header("Marker Cluster")
+old=pd.read_csv('https://github.com/cw75964/GIS-finals/raw/refs/heads/master/%E5%8F%B0%E7%81%A3%E5%8F%A4%E8%B9%9F%20%E4%BF%AE%E6%AD%A3.csv')
+nation_old=old=old[old['assetsClassifyName']=='縣(市)定古蹟']
+m1 = leafmap.Map(center=[23.7652,120.4980],zoom=8,
+            locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
+m1.add_basemap("OpenTopoMap")
+m1.add_points_from_xy(nation_old,x='longitude',y='latitude',spin=True,add_legend=True,layer_name='縣(市)定古蹟')
+m1.to_streamlit(height=700)
+st.dataframe(nation_old)
+st.header("Heatmap")
+nation_old['num']=10
+m2 = leafmap.Map(center=[23.7652,120.4980],zoom=8,
+            locate_control=True, latlon_control=True, draw_export=True, minimap_control=True
+            )
+m2.add_basemap("OpenTopoMap")
+m2.add_heatmap(nation_old,
+              latitude="latitude",longitude="longitude",value="num",name="Heat map",radius=20,)
+m2.to_streamlit(height=700)
