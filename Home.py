7af46c7ee1e0cade0dfe487d8235101002cd960a
cw75964 <cw75964@gmail.com>
2024-12-21 14:38:40 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -18,11 +18,11 @@ logo = "https://i.imgur.com/UbOXYAU.png"
 st.sidebar.image(logo)
 
 # Customize page title
-st.header('古蹟、歷史建築之定義')
+st.title('古蹟、歷史建築之定義')
 st.write('古蹟：指人類為生活需要所營建之具有歷史、文化、藝術價值之建造物及附屬設施。')
 st.write('歷史建築：指歷史事件所定著或具有歷史性、地方性、特殊性之文化、藝術價值，應予保存之建造物及附屬設施。')
 
-st.header("臺灣古蹟、歷史建築之分布")
+st.title("臺灣古蹟、歷史建築之分布")
 
 old=pd.read_csv('https://github.com/cw75964/GIS-finals/raw/refs/heads/master/%E5%8F%B0%E7%81%A3%E5%8F%A4%E8%B9%9F%20%E4%BF%AE%E6%AD%A3.csv')
 building=pd.read_csv('https://github.com/cw75964/GIS-finals/raw/refs/heads/master/%E5%8F%B0%E7%81%A3%E6%AD%B7%E5%8F%B2%E5%BB%BA%E7%AF%89%20%E4%BF%AE%E6%AD%A3.csv')
