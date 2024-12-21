01d86cd8661a5353bb95fa5733468dec3b88b92b
cw75964 <cw75964@gmail.com>
2024-12-21 18:21:57 +0800

    Update 1臺灣國定古蹟.py

--- pages/1臺灣國定古蹟.py
+++ pages/1臺灣國定古蹟.py
@@ -5,30 +5,34 @@ import pandas as pd
 
 st.set_page_config(layout="wide")
 
-st.title("臺灣國定古蹟")
+st.title("古蹟級別選單")
 
-st.header("Marker Cluster")
-old=pd.read_csv('https://github.com/cw75964/GIS-finals/raw/refs/heads/master/%E5%8F%B0%E7%81%A3%E5%8F%A4%E8%B9%9F%20%E4%BF%AE%E6%AD%A3.csv')
-old_na=pd.read_csv('https://github.com/cw75964/GIS-finals/raw/refs/heads/master/%E5%8F%A4%E8%B9%9F%E7%94%A8%E9%80%94%E3%80%81%E7%B8%A3%E5%B8%82.csv')
-nation_na=old_na[old_na['assetsClassifyName']=='國定古蹟']
-nation_name=nation_na['name'].value_counts()
-nation_city=nation_na['city'].value_counts()
+old_na=pd.read_csv('https://github.com/cw75964/GIS-finals/raw/refs/heads/master/%E5%8F%A4%E8%B9%9F%E7%94%A8%E9%80%94%E3%80%81%E7%B8%A3%E5%B8%82%E3%80%81%E5%9C%B0%E5%9D%80.csv')
+build_na=pd.read_csv('https://github.com/cw75964/GIS-finals/raw/refs/heads/master/%E6%AD%B7%E5%8F%B2%E5%BB%BA%E7%AF%89%E7%94%A8%E9%80%94%E3%80%81%E7%B8%A3%E5%B8%82%E3%80%81%E5%9C%B0%E5%9D%80.csv')
+old_na=old_na.drop(['addresses','num'],axis=1)
+build_na=build_na.drop(['addresses','num'],axis=1)
 
-nation_old=old=old[old['assetsClassifyName']=='國定古蹟']
+lv_option_list = old_na["assetsClassifyName"].unique().tolist()
+lv_option = st.multiselect("選擇級別", lv_option_list)
+
+if city_option:
+            old_lv_filtered = old_na[old_na["assetsClassifyName"].isin(lv_option)]
+else:
+            old_lv_filtered = old_na
+st.subheader("臺灣古蹟 Marker Cluster")
 m1 = leafmap.Map(center=[23.7652,120.4980],zoom=8,
             locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
 m1.add_basemap("OpenTopoMap")
-m1.add_points_from_xy(nation_old,x='longitude',y='latitude',spin=True,add_legend=True,layer_name='國定古蹟')
+m1.add_points_from_xy(old_lv_filtered,x='longitude',y='latitude',spin=True,add_legend=True,layer_name='臺灣古蹟')
 m1.to_streamlit(height=700)
-st.dataframe(nation_old)
-st.bar_chart(nation_name)
-st.header("Heatmap")
-nation_old['num']=10
-m2 = leafmap.Map(center=[23.7652,120.4980],zoom=8,
-            locate_control=True, latlon_control=True, draw_export=True, minimap_control=True
-            )
-m2.add_basemap("OpenTopoMap")
-m2.add_heatmap(nation_old,
-              latitude="latitude",longitude="longitude",value="num",name="Heat map",radius=20,)
+st.subheader("資料表")
+st.dataframe(old_lv_filtered)
+st.subheader("以過去用途統計之長條圖")
+old_lv_use=old_lv_filtered['name'].value_counts()
+st.bar_chart(old_lv_filtered)
+st.subheader("臺灣古蹟分布 Heatmap")
+old_lv_filtered['num']=10
+m2 = leafmap.Map(center=[23.7652,120.4980],zoom=8,locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
+m2.add_heatmap(old_lv_filtered,latitude="latitude",longitude="longitude",value="num",name="古蹟分布Heat map",radius=15,)
 m2.to_streamlit(height=700)
-st.bar_chart(nation_city)
+
