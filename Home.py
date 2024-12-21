fba979f4f2b1d8c76cd1d266907dc0d5ff7809e6
cw75964 <cw75964@gmail.com>
2024-12-22 01:27:52 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -17,7 +17,7 @@ st.title("臺灣古蹟、歷史建築之分布")
 
 old_na=pd.read_csv('https://github.com/cw75964/GIS-finals/raw/refs/heads/master/%E5%8F%A4%E8%B9%9F%E7%94%A8%E9%80%94%E3%80%81%E7%B8%A3%E5%B8%82%E3%80%81%E5%9C%B0%E5%9D%80.csv')
 build_na=pd.read_csv('https://github.com/cw75964/GIS-finals/raw/refs/heads/master/%E6%AD%B7%E5%8F%B2%E5%BB%BA%E7%AF%89%E7%94%A8%E9%80%94%E3%80%81%E7%B8%A3%E5%B8%82%E3%80%81%E5%9C%B0%E5%9D%80.csv')
-old_na=old_na.drop(['addresses','num'],axis=1)
+old_na=old_na.drop(['num'],axis=1)
 build_na=build_na.drop(['num'],axis=1)
 
 
