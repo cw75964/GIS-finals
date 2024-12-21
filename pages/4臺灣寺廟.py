a864517a81acded47b06acc3d772c23381661c62
cw75964 <cw75964@gmail.com>
2024-12-21 21:27:47 +0800

    Update 4臺灣寺廟.py

--- pages/4臺灣寺廟.py
+++ pages/4臺灣寺廟.py
@@ -35,7 +35,6 @@ if len(name_option) > 0:
 # 如果用户选择了资产级别，就过滤资产级别
 if len(lv_option) > 0:
             filtered_old = filtered_old[filtered_old["assetsClassifyName"].isin(lv_option)]
-            filtered_build = filtered_build
 
 col1, col2 = st.columns([1, 1])
 with col1:
@@ -52,5 +51,19 @@ with col1:
             m2 = leafmap.Map(center=[23.7652,120.4980],zoom=8,locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
             m2.add_heatmap(filtered_old,latitude="latitude",longitude="longitude",value="num",name="古蹟分布Heat map",radius=15,)
             m2.to_streamlit(height=700)
+with col2:
+            st.subheader("臺灣歷史建築 Marker Cluster")
+            m3 = leafmap.Map(center=[23.7652,120.4980],zoom=8,
+                        locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
+            m3.add_basemap("OpenTopoMap")
+            m3.add_points_from_xy(filtered_build,x='longitude',y='latitude',spin=True,add_legend=True,layer_name='臺灣歷史建築')
+            m3.to_streamlit(height=700)
+            st.subheader("資料表")
+            st.dataframe(filtered_build)
+            filtered_build['num']=10
+            m4 = leafmap.Map(center=[23.7652,120.4980],zoom=8,locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
+            m4.add_heatmap(filtered_build,latitude="latitude",longitude="longitude",value="num",name="歷史建築分布Heat map",radius=15,)
+            m4.to_streamlit(height=700)
             
+
 
