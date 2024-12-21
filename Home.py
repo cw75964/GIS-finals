d5c696db8a288738e9b7931ba4b42c784863437b
cw75964 <cw75964@gmail.com>
2024-12-21 17:32:10 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -28,8 +28,8 @@ old=pd.read_csv('https://github.com/cw75964/GIS-finals/raw/refs/heads/master/%E5
 building=pd.read_csv('https://github.com/cw75964/GIS-finals/raw/refs/heads/master/%E5%8F%B0%E7%81%A3%E6%AD%B7%E5%8F%B2%E5%BB%BA%E7%AF%89%20%E4%BF%AE%E6%AD%A3.csv')
 old_na=pd.read_csv('https://github.com/cw75964/GIS-finals/raw/refs/heads/master/%E5%8F%A4%E8%B9%9F%E7%94%A8%E9%80%94%E3%80%81%E7%B8%A3%E5%B8%82%E3%80%81%E5%9C%B0%E5%9D%80.csv')
 build_na=pd.read_csv('https://github.com/cw75964/GIS-finals/raw/refs/heads/master/%E6%AD%B7%E5%8F%B2%E5%BB%BA%E7%AF%89%E7%94%A8%E9%80%94%E3%80%81%E7%B8%A3%E5%B8%82%E3%80%81%E5%9C%B0%E5%9D%80.csv')
-old_na.drop(columns=['addresses'])
-build_na.drop(columns=['addresses'])
+old_na.drop(columns=['addresses','Unnamed'])
+build_na.drop(columns=['addresses','Unnamed'])
 
 old_name=old_na['name'].value_counts()
 old_city=old_na['city'].value_counts()
