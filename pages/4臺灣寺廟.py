6b455478f3e287b25f2155c9ad775b2a5dfc4a0a
cw75964 <cw75964@gmail.com>
2024-12-21 21:15:59 +0800

    Update 4臺灣寺廟.py

--- pages/4臺灣寺廟.py
+++ pages/4臺灣寺廟.py
@@ -23,26 +23,34 @@ with ocol3:
             lv_option_list = old_na["assetsClassifyName"].unique().tolist()
             lv_option = st.multiselect("選擇級別", lv_option_list,default=[])
 filtered_old = old_na
-
+filtered_build=build_na
 # 如果用户选择了城市，就过滤城市
 if len(city_option) > 0:
-    filtered_old = filtered_old[filtered_old["city"].isin(city_option)]
-
+            filtered_old = filtered_old[filtered_old["city"].isin(city_option)]
+            filtered_build = filtered_build[filtered_build["city"].isin(city_option)]
 # 如果用户选择了过去用途，就过滤过去用途
 if len(name_option) > 0:
-    filtered_old = filtered_old[filtered_old["name"].isin(name_option)]
-
+            filtered_old = filtered_old[filtered_old["name"].isin(name_option)]
+            filtered_build = filtered_build[filtered_build["name"].isin(name_option)]
 # 如果用户选择了资产级别，就过滤资产级别
 if len(lv_option) > 0:
-    filtered_old = filtered_old[filtered_old["assetsClassifyName"].isin(lv_option)]
+            filtered_old = filtered_old[filtered_old["assetsClassifyName"].isin(lv_option)]
+            filtered_build = filtered_build[filtered_build["assetsClassifyName"].isin(lv_option)]
 
+col1, col2 = st.columns([1, 1])
+with col1:
+            st.subheader("臺灣古蹟 Marker Cluster")
+            m1 = leafmap.Map(center=[23.7652,120.4980],zoom=8,
+                        locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
+            m1.add_basemap("OpenTopoMap")
+            m1.add_points_from_xy(filtered_old,x='longitude',y='latitude',spin=True,add_legend=True,layer_name='臺灣古蹟')
+            m1.to_streamlit(height=700)
+            st.subheader("資料表")
+            st.dataframe(filtered_old)
+            st.subheader("臺灣古蹟分布 Heatmap")
+            filtered_old['num']=10
+            m2 = leafmap.Map(center=[23.7652,120.4980],zoom=8,locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
+            m2.add_heatmap(filtered_old,latitude="latitude",longitude="longitude",value="num",name="古蹟分布Heat map",radius=15,)
+            m2.to_streamlit(height=700)
+            
 
-
-st.subheader("臺灣古蹟 Marker Cluster")
-m1 = leafmap.Map(center=[23.7652,120.4980],zoom=8,
-            locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
-m1.add_basemap("OpenTopoMap")
-m1.add_points_from_xy(filtered_old,x='longitude',y='latitude',spin=True,add_legend=True,layer_name='臺灣古蹟')
-m1.to_streamlit(height=700)
-st.subheader("資料表")
-st.dataframe(filtered_old)
