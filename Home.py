27a50b9116b28e013b9324fc3321600f1d99365f
cw75964 <cw75964@gmail.com>
2024-12-21 17:01:38 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -34,41 +34,46 @@ old_city=old_na['city'].value_counts()
 build_name=build_na['name'].value_counts()
 build_city=build_na['city'].value_counts()
 
-st.header("臺灣古蹟 Marker Cluster")
-m1 = leafmap.Map(center=[23.7652,120.4980],zoom=8,
-            locate_control=True, latlon_control=True, draw_export=True, minimap_control=True
-            )
-m1.add_basemap("OpenTopoMap")
-m1.add_points_from_xy(old,x='longitude',y='latitude',spin=True,add_legend=True,layer_name='臺灣古蹟')
-m1.to_streamlit(height=700)
-st.write(old)
-st.bar_chart(old_name)
+city_option_list = old_na["city"].unique().tolist()
+city_option = st.multiselect("選擇縣市", city_option_list)
 
-st.header("臺灣古蹟分布 Heatmap")
-with st.expander("See source code"):
-    with st.echo():
-        old['num']=10
-        m2 = leafmap.Map(center=[23.7652,120.4980],zoom=8,locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
-        m2.add_basemap("OpenTopoMap")
-        m2.add_heatmap(old,latitude="latitude",longitude="longitude",value="num",name="古蹟分布Heat map",radius=15,)
-m2.to_streamlit(height=700)
-st.bar_chart(old_city)
+if city_option:
+            old_city_filtered = old_na[old_na["city"].isin(city_option)]
+            build_city_filtered=build_na[build_na["city"].isin(city_option)]            
+else:
+            old_city_filtered = old_na
+            build_city_filtered=build_na[build_na["city"].isin(city_option)]
 
-st.header("臺灣歷史建築 Marker Cluster")
-m3 = leafmap.Map(center=[23.7652,120.4980],zoom=8,
-            locate_control=True, latlon_control=True, draw_export=True, minimap_control=True
-            )
-m3.add_basemap("OpenTopoMap")
-m3.add_points_from_xy(building,x='longitude',y='latitude',spin=True,add_legend=True,layer_name='臺灣歷史建築')
-m3.to_streamlit(height=700)
-st.write(building)
-st.bar_chart(build_name)
-st.header("臺灣歷史建築分布 Heatmap")
-with st.expander("See source code"):
-    with st.echo():
-        building['num']=10
-        m4 = leafmap.Map(center=[23.7652,120.4980],zoom=8,locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
-        m4.add_basemap("OpenTopoMap")
-        m4.add_heatmap(building,latitude="latitude",longitude="longitude",value="num",name="歷史建築分布Heat map",radius=15,)
-m4.to_streamlit(height=700)
-st.bar_chart(build_city)
+with col1:
+            st.subheader("臺灣古蹟 Marker Cluster")
+            m1 = leafmap.Map(center=[23.7652,120.4980],zoom=8,
+                        locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
+            m1.add_basemap("OpenTopoMap")
+            m1.add_points_from_xy(old_city_filtered,x='longitude',y='latitude',spin=True,add_legend=True,layer_name='臺灣古蹟')
+            m1.to_streamlit(height=700)
+            st.dataframe(old_city_filtered)
+            old_city_name=old_city_filtered['name'].value_counts()
+            st.bar_chart(old_city_name)
+            st.subheader("臺灣古蹟分布 Heatmap")
+            old_city_filtered['num']=10
+            m2 = leafmap.Map(center=[23.7652,120.4980],zoom=8,locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
+            m2.add_heatmap(old_city_filtered,latitude="latitude",longitude="longitude",value="num",name="古蹟分布Heat map",radius=15,)
+            m2.to_streamlit(height=700)
+            st.bar_chart(old_city)
+
+with col2:
+            st.subheader("臺灣歷史建築 Marker Cluster")
+            m3 = leafmap.Map(center=[23.7652,120.4980],zoom=8,
+                        locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
+            m3.add_basemap("OpenTopoMap")
+            m3.add_points_from_xy(build_city_filtered,x='longitude',y='latitude',spin=True,add_legend=True,layer_name='臺灣歷史建築')
+            m3.to_streamlit(height=700)
+            st.write(build_city_filtered)
+            build_city_name=build_city_filtered['name'].value_counts()
+            st.bar_chart(build_city_name)
+            st.header("臺灣歷史建築分布 Heatmap")
+            build_city_filtered['num']=10
+            m4 = leafmap.Map(center=[23.7652,120.4980],zoom=8,locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
+            m4.add_heatmap(build_city_filtered,latitude="latitude",longitude="longitude",value="num",name="歷史建築分布Heat map",radius=15,)
+            m4.to_streamlit(height=700)
+            st.bar_chart(build_city)
